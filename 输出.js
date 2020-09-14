d0bf6c8ef6a613c7022cf8b4204f4ea05ac0215a
1565163445@qq.com <1565163445@qq.com>
2020-09-14 17:36:40 +0800

    新增

--- 输出.js
+++ 输出.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-08 10:57:22
- * @LastEditTime: 2020-09-12 22:26:12
+ * @LastEditTime: 2020-09-14 16:24:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \小卡车的博客\3.排序算法\输出.js
@@ -59,3 +59,97 @@ function inner() {
   console.log(inner.caller);
 }
 outer();
+
+// arguments
+function f() {
+  console.log(arguments);
+  console.log(typeof arguments);
+  for (var key in arguments) {
+    console.log(arguments[key]);
+  }
+}
+f(1, 2, 3);
+
+// bind
+function a() {
+  console.log("a");
+}
+var obj = new Object();
+console.log(obj);
+
+console.log("----------------------");
+// defineProperty
+// var msg = {
+//   name: "xkc",
+//   age: 20,
+// };
+// Object.defineProperty(msg, "name", {
+// /*   configurable: true,
+//   enumerable: true, */
+//   get: function () {
+//     return "666";
+//   },
+//   set: function (newValue) {
+//     if (newValue) {
+//       this.name = "XKC";
+//     }
+//   },
+// });
+// msg.name = 12;
+// console.log(msg.name);
+var book = {
+  _year: 2004,
+  edit: 1,
+};
+Object.defineProperty(book, "year", {
+  get: function () {
+    return this._year || 2;
+  },
+  set: function (newValue) {
+    if (newValue > 2004) {
+      this._year = newValue;
+      this.edit += newValue - 2004;
+    }
+  },
+});
+book.year = 2005;
+console.log(book.year);
+
+// class
+console.log('-------------------------------')
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  write(i) {
+    console.log(i);
+  }
+  
+  say() {
+    console.log(`Hello World ${this.name}`);
+  }
+  static hai() {
+    console.log(`hai`);
+  }
+}
+var person = new Person("小卡车", 20);
+Person.hai();
+person.say();
+class XKC extends Person{
+  constructor(name, age, habby) {
+    super(name, age)
+    this.habby = habby
+  }
+  write() {
+    console.log(arguments);
+  }
+  
+  say() {
+    console.log(`${this.name}，今年${this.age}岁，${super.name} 在说话`);
+  }
+}
+var xkc = new XKC('飞天小卡车', 20, '打LOL');
+xkc.say();
+xkc.write(1);
+xkc.write(1, 2);
