b82c45e893bc7c88048b6dbf9650c89dad8c19f3
1565163445@qq.com <1565163445@qq.com>
2020-09-19 11:18:41 +0800

    +

---
+++ ApplicationCache/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-19 10:34:57
+ * @LastEditTime: 2020-09-19 10:40:31
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \小卡车的博客\3.排序算法\ApplicationCache\index.js
+ */
+window.onload = function() {
+  console.log(1)
+}
